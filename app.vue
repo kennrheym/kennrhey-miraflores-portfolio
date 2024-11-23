46ba6746cc67a93f6b45476cda473cde125a1bfd
KENNRHEYM\63945 <ken.m@daelibs.com.au>
2024-11-24 00:29:06 +0800

    test

--- app.vue
+++ app.vue
@@ -6,17 +6,45 @@ import FixedMainHeader from '@/components/layout/main-header.vue'
   <div>
     <fixed-main-header></fixed-main-header>
     <main>
-      <div class="flex--all-centered">
-        <div>
+      <div class="flex--all-centered" style="margin-top: 56px;">
+        <div style="padding-top: 20px;">
           <paragraph> Hi, my name is Kennrhey! I'm a full-stack developer</paragraph>
         </div>
         <div>
 
-          <div class="test-container">
+          <!-- <div class="test-container">
             <div class="test" style="background-color: white; height: 20px; width: 20px; border-radius: 10px;">
               
             </div>
+          </div> -->
+          <div style="height: 100px;">
+
           </div>
+          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>          <div style="height: 100px;">
+
+</div>
         </div>
 
       </div>
@@ -42,12 +70,18 @@ import FixedMainHeader from '@/components/layout/main-header.vue'
 
   html {
     background-color: main.$app-dark-mode--background-color;
+    // background: linear-gradient(0, white, #121212);
+
   }
 
   main {
-    margin-top: 56px;
+    overflow: auto;
+    height: calc(100vh - 56px);
+    background-color: transparent;
+    background: linear-gradient(0, white, #121212);
     & > div {
-      background-color: main.$app-dark-mode--background-color;
+      // background-color: main.$app-dark-mode--background-color;
+      background-color: transparent;
     }
   }
 
