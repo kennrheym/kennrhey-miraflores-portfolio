2889f8ca91c5b812f308ef4a8ed63e0dc683dd21
KENNRHEYM\63945 <ken.m@daelibs.com.au>
2024-11-25 16:29:26 +0800

    UI: Carrousel

--- components/layout/main-content.vue
+++ components/layout/main-content.vue
@@ -1,32 +1,42 @@
 <script setup lang="ts">
+import Carrousel from '../ui/custom/carrousel/carrousel.vue';
+
+Carrousel
 </script>
 
 <template>
-    <div style="margin-left: 6px;">
-        <div style="font-size: 48px;">
-            <div>Hi, my</div>
-            <div>name is <span style="color: #ace5cb">Kennrhey</span></div>
-        </div>
-        <div style="margin-top: 22px;" >
-            <div style="font-size: 20px;">
-                I'm a full-stack web developer
+    <div id="main-content-container">
+        <div style="margin-left: 6px;">
+            <div style="font-size: 48px;">
+                <div>Hi, my</div>
+                <div>name is <span style="color: #ace5cb">Kennrhey</span></div>
             </div>
-            <div style="font-size: 13px; margin-top: 5px;">
-                <div>
-                    And I'm really passionate with...
+            <div style="margin-top: 22px;" >
+                <div style="font-size: 20px;">
+                    I'm a full-stack web developer
                 </div>
-                <div>
-                    Creating front-end components, and implementing business logic
-                </div>   
+                <div style="font-size: 13px; margin-top: 5px;">
+                    <div>
+                        And I'm really passionate with...
+                    </div>
+                    <div>
+                        Creating front-end components, and implementing business logic
+                    </div>   
+                </div>
             </div>
-         
-
-            
-
+        </div>
+        <div style="margin-left: 100px;">
+            <carrousel></carrousel>
         </div>
     </div>
 
 </template>
 
 <style lang="scss">
+    #main-content-container {
+        @include main.flex--all-centered;
+        display: flex;
+        flex-direction: row;
+    }
+
 </style>
---
+++ components/ui/custom/carrousel/carrousel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div id="carrousel-container">
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <polyline points="15 18 9 12 15 6"></polyline>
+            </svg>
+        </div>
+        <div>
+            <div>
+
+            </div>
+        </div>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <polyline points="9 6 15 12 9 18"></polyline>
+            </svg>
+        </div>
+    </div>
+
+</template>
+<style lang="scss">
+    #carrousel-container {
+        @include main.flex--all-centered;
+        display: flex;
+        flex-direction: row;
+    }
+</style>
