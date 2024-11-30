b63c5fcc506c93e7bb419045f95e92b6036ee8bf
KENNRHEYM\63945 <ken.m@daelibs.com.au>
2024-12-01 01:40:25 +0800

    UI: left drawer (WIP)

--- components/layout/menu-drawer.vue
+++ components/layout/menu-drawer.vue
@@ -8,14 +8,15 @@ const isOpen = defineModel('isOpen', {
 </script>
 
 <template>
-    <div v-if="isOpen" class="overlay">
-        <div class="drawer">
+    <div>
+        <div class="drawer" :class="{'drawer--open': isOpen }">
             <div id="top-row">
                 <div id="container--icon-menu" class="clickable" @click="isOpen=false">
                     <img src="/assets/svg/icon--menu.svg" />
                 </div>
             </div>
-
+        </div>
+        <div  v-if="isOpen" class="overlay">
         </div>
     </div>
 </template>
@@ -42,14 +43,18 @@ const isOpen = defineModel('isOpen', {
 .drawer {
   position: fixed;
   top: 0;
-  left: 0;
+  left: -250px;
   width: 250px;
   height: 100%;
   background-color: #333;
   color: #fff;
-  transition: left 0.3s ease;
+  transition: left 0.2s ease;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.5);
-  z-index: 1000;
+  z-index: 10000;
+}
+
+.drawer--open {
+    left: 0 !important;
 }
 
 .overlay {
