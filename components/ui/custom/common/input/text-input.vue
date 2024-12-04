ecf6d02656d8b1f1d8081eb4331923804b65ab63
KENNRHEYM\63945 <ken.m@daelibs.com.au>
2024-12-04 14:50:29 +0800

    WIP: input components

---
+++ components/ui/custom/common/input/text-input.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+enum InputTypeEnum {
+    TEXT = 'text',
+    DATE = 'date',
+    CHECKBOX = 'checkbox'
+}
+</script>
+<template>
+
+</template>
+<style lang='scss' scoped>
+</style>
